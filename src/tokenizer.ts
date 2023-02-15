79650a6b9b9d8a7031b8b15a02dd51271aaf5b04
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-02-15 03:22:22 -0600

    chore(tokenizer): cleanup

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -15,6 +15,7 @@ export function tokenize(code: string, index: number = 0): Token[] {
 
   while (index < code.length) {
     let char = code[index]
+    let value = ''
 
     // Skip whitespace
     if (/\s/.test(char)) {
@@ -22,44 +23,33 @@ export function tokenize(code: string, index: number = 0): Token[] {
       continue
     }
 
+    // Escape newlines after directives, skip comments
+    if (char === '#') {
+      let j = index
+      while (code[j] !== '\n' && !/\/[\/\*]/.test(code[j] + code[j + 1])) j++
+      code = code.substring(0, j) + '\\' + code.substring(j)
+    }
+
+    // Parse values and identifiers
+    while (/\w/.test(code[index])) value += code[index++]
+
     // Parse symbols, combine if able
-    if (!/\w/.test(char)) {
-      let value = char
+    if (!value) {
+      value = char
       for (const symbol of GLSL_SYMBOLS) {
         if (symbol.length > value.length && code.startsWith(symbol, index)) value = symbol
       }
       index += value.length
-
-      // Preserve whitespace in comments
-      if (/\/[\/\*]/.test(value)) {
-        const isMultiline = value === '/*'
-        while (!value.endsWith(isMultiline ? '*/' : '\n')) {
-          value += code[index]
-          index++
-        }
-        tokens.push({ type: 'comment', value })
-      } else {
-        // Escape newlines after directives, skip comments
-        if (char === '#') {
-          let i = index
-          while (char !== '\n' && !/\/[\/\*]/.test(code[i] + code[i + 1])) char = code[i++]
-          code = code.substring(0, i - 1) + '\\' + code.substring(i)
-        }
-
-        tokens.push({ type: 'symbol', value })
-      }
-
-      continue
     }
 
-    // Parse alphanumeric tokens
-    let value = ''
-    while (/\w/.test(code[index])) {
-      value += code[index]
-      index++
-    }
-
-    if (/\d/.test(char)) {
+    // Label and add token
+    if (/\/[\/\*]/.test(value)) {
+      const isMultiline = value === '/*'
+      while (!value.endsWith(isMultiline ? '*/' : '\n')) value += code[index++]
+      tokens.push({ type: 'comment', value })
+    } else if (!/\w/.test(char)) {
+      tokens.push({ type: 'symbol', value })
+    } else if (/\d/.test(char)) {
       tokens.push({ type: 'number', value })
     } else if (GLSL_RESERVED.includes(value)) {
       tokens.push({ type: 'reserved', value })
