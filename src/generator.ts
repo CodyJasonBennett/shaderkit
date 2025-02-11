630f249f4739ceb4c8aa97dd21d6780747d46822
Cody Bennett <hi@codyb.co>
2025-02-10 18:33:04 -0600

    fix(generator): don't add whitespace

--- src/generator.ts
+++ src/generator.ts
@@ -5,7 +5,7 @@ function formatLayout(layout: Record<string, string | boolean> | null): string {
 
   return `layout(${Object.entries(layout)
     .map(([k, v]) => (v === true ? k : `${k}=${v}`))
-    .join(', ')}) `
+    .join(',')})`
 }
 
 // TODO: restore comments/whitespace with sourcemaps, WGSL support
@@ -18,7 +18,7 @@ function format(node: AST | null): string {
     case 'Literal':
       return node.value
     case 'ArraySpecifier':
-      return `${node.typeSpecifier.name}[${node.dimensions.map(format).join(', ')}]`
+      return `${node.typeSpecifier.name}[${node.dimensions.map(format).join(',')}]`
     case 'ExpressionStatement':
       return `${format(node.expression)};`
     case 'BlockStatement':
@@ -28,8 +28,8 @@ function format(node: AST | null): string {
     case 'PreprocessorStatement': {
       let value = ''
       if (node.value) {
-        if (node.name === 'include') value = ` <${format(node.value[0])}>`
-        else if (node.name === 'extension') value = ` ${node.value.map(format).join(': ')}`
+        if (node.name === 'include') value = ` <${format(node.value[0])}>` // three is whitespace sensitive
+        else if (node.name === 'extension') value = `${node.value.map(format).join(':')}`
         else value = ` ${node.value.map(format).join(' ')}`
       }
 
@@ -44,25 +44,25 @@ function format(node: AST | null): string {
     case 'ContinueStatement':
       return 'continue;'
     case 'IfStatement': {
-      const alternate = node.alternate ? ` else ${format(node.consequent)}` : ''
-      return `if (${format(node.test)}) ${format(node.consequent)}${alternate}`
+      const alternate = node.alternate ? ` else${format(node.consequent)}` : ''
+      return `if(${format(node.test)})${format(node.consequent)}${alternate}`
     }
     case 'SwitchStatement':
-      return `switch (${format(node.discriminant)}) {${node.cases.map(format).join('')}}`
+      return `switch(${format(node.discriminant)}){${node.cases.map(format).join('')}}`
     case 'SwitchCase':
-      return `case ${node.test ? format(node.test) : 'default'}: {${node.consequent.map(format).join(';')}}`
+      return `case ${node.test ? format(node.test) : 'default'}:{${node.consequent.map(format).join(';')}}`
     case 'WhileStatement':
       return `while (${format(node.test)}) ${format(node.body)}`
     case 'DoWhileStatement':
-      return `do ${format(node.body)} while (${format(node.test)})`
+      return `do ${format(node.body)}while(${format(node.test)})`
     case 'ForStatement':
-      return `for (${format(node.init)};${format(node.test)};${format(node.update)}) ${format(node.body)}`
+      return `for(${format(node.init)};${format(node.test)};${format(node.update)})${format(node.body)}`
     case 'FunctionDeclaration': {
       const qualifiers = node.qualifiers.length ? `${node.qualifiers.join(' ')} ` : '' // precision
-      const body = node.body ? ` ${format(node.body)}` : ';'
+      const body = node.body ? format(node.body) : ';'
       return `${qualifiers}${format(node.typeSpecifier)} ${format(node.id)}(${node.params
         .map(format)
-        .join(', ')})${body}`
+        .join(',')})${body}`
     }
     case 'FunctionParameter': {
       const qualifiers = node.qualifiers.length ? `${node.qualifiers.join(' ')} ` : ''
@@ -72,37 +72,37 @@ function format(node: AST | null): string {
       const head = node.declarations[0]
       const layout = formatLayout(head.layout)
       const qualifiers = head.qualifiers.length ? `${head.qualifiers.join(' ')} ` : ''
-      return `${layout}${qualifiers}${format(head.typeSpecifier)} ${node.declarations.map(format).join(', ')};`
+      return `${layout}${qualifiers}${format(head.typeSpecifier)} ${node.declarations.map(format).join(',')};`
     }
     case 'VariableDeclarator': {
-      const init = node.init ? ` = ${format(node.init)}` : ''
+      const init = node.init ? `=${format(node.init)}` : ''
       return `${format(node.id)}${init}`
     }
     case 'UniformDeclarationBlock': {
       const layout = formatLayout(node.layout)
       const qualifiers = node.qualifiers.length ? `${node.qualifiers.join(' ')} ` : ''
-      const scope = node.id ? ` ${format(node.id)}` : ''
-      return `${layout}${qualifiers}${format(node.typeSpecifier)} {${node.members.map(format).join('')}}${scope};`
+      const scope = node.id ? `${format(node.id)}` : ''
+      return `${layout}${qualifiers}${format(node.typeSpecifier)}{${node.members.map(format).join('')}}${scope};`
     }
     case 'StructDeclaration':
-      return `struct ${format(node.id)} {${node.members.map(format).join('')}};`
+      return `struct ${format(node.id)}{${node.members.map(format).join('')}};`
     case 'ArrayExpression':
-      return `${format(node.typeSpecifier)}(${node.elements.map(format).join(', ')})`
+      return `${format(node.typeSpecifier)}(${node.elements.map(format).join(',')})`
     case 'UnaryExpression':
     case 'UpdateExpression':
       return node.prefix ? `${node.operator}${format(node.argument)}` : `${format(node.argument)}${node.operator}`
     case 'BinaryExpression':
     case 'AssignmentExpression':
     case 'LogicalExpression':
-      return `${format(node.left)} ${node.operator} ${format(node.right)}`
+      return `${format(node.left)}${node.operator}${format(node.right)}`
     case 'MemberExpression':
       return node.computed
         ? `${format(node.object)}[${format(node.property)}]`
         : `${format(node.object)}.${format(node.property)}`
     case 'ConditionalExpression':
-      return `${format(node.test)} ? ${format(node.consequent)} : ${format(node.alternate)}`
+      return `${format(node.test)}?${format(node.consequent)}:${format(node.alternate)}`
     case 'CallExpression':
-      return `${format(node.callee)}(${node.arguments.map(format).join(', ')})`
+      return `${format(node.callee)}(${node.arguments.map(format).join(',')})`
     case 'Program':
       return `${node.body.map(format).join('')}`
     default:
@@ -118,5 +118,5 @@ export interface GenerateOptions {
  * Generates a string of GLSL (WGSL WIP) code from an [AST](https://en.wikipedia.org/wiki/Abstract_syntax_tree).
  */
 export function generate(program: Program, options: GenerateOptions): string {
-  return format(program).replaceAll('\n\n', '\n') // NOTE: strips excess whitespace from chained preprocessor
+  return '#version 300 es\n' + format(program).replaceAll('\n\n', '\n')
 }
--- tests/generator.test.ts
+++ tests/generator.test.ts
@@ -1,34 +1,22 @@
 import { generate, parse } from 'shaderkit'
 import { describe, it, expect } from 'vitest'
 
-function test(code: string): void {
-  const program = parse(code + '\n')
-  const output = generate(program, { target: 'GLSL' })
-  expect(output.trim()).toBe(code.trim())
-}
+const glsl = /* glsl */ `#version 300 es
+precision mediump float;
+#define PLUS(n) n+=1
+#define TEST
+#if defined(TEST)
+const bool isTest=true;
+#endif
+uniform float foo,bar;uniform sampler2D map;in vec2 vUv;out vec4 pc_FragColor;
+#include <three_test>
+layout(std140)uniform Uniforms1{mat4 projectionMatrix;mat4 modelViewMatrix;mat3 normalMatrix;float one,two;};layout(std140)uniform Uniforms2{mat4 projectionMatrix;mat4 modelViewMatrix;mat3 normalMatrix;float one,two;}globals;struct LightData{float intensity;vec3 position;float one,two;};uniform LightData Light[4];void main(){vec4 lightNormal=vec4(Light[0].position.xyz*Light[0].intensity,0.0);vec4 clipPosition=projectionMatrix*modelViewMatrix*vec4(0,0,0,1);vec4 clipPositionGlobals=globals.projectionMatrix*globals.modelViewMatrix*vec4(0,0,0,1);if(false){}pc_FragColor=vec4(texture(map,vUv).rgb,0.0);float bar=0.0;pc_FragColor.a+=1.0+bar;}
+`.trim()
 
 describe('generator', () => {
-  it('can handle preprocessor statements', () => {
-    // test('#version 300 es')
-    test('#define PLUS(n) n += 1')
-    test('#define TEST')
-    test('#if defined(TEST)\nconst bool isTest = true;\n#endif')
-  })
-
-  it('can handle structs', () => {
-    test('struct LightData {float intensity;vec3 position;float one, two;};')
-  })
-
-  it('can handle suffix array specifiers', () => {
-    test('uniform LightData Light[4];')
-  })
-
-  it('can handle uniform declaration blocks', () => {
-    test(
-      'layout(std140) uniform Uniforms1 {mat4 projectionMatrix;mat4 modelViewMatrix;mat3 normalMatrix;float one, two;};',
-    )
-    test(
-      'layout(std140) uniform Uniforms2 {mat4 projectionMatrix;mat4 modelViewMatrix;mat3 normalMatrix;float one, two;} globals;',
-    )
+  it('can handle GLSL', () => {
+    const program = parse(glsl)
+    const output = generate(program, { target: 'GLSL' })
+    expect(output).toBe(glsl)
   })
 })
