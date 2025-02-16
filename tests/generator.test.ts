28ac487ed57df35fd974f7d6f066ea3bf1626c60
Cody Bennett <hi@codyb.co>
2025-02-16 00:27:27 -0600

    chore: add ES 3.1 compute test

--- tests/generator.test.ts
+++ tests/generator.test.ts
@@ -86,10 +86,63 @@ const glsl = /* glsl */ `#version 300 es
   }
 `
 
+// https://community.arm.com/arm-community-blogs/b/mobile-graphics-and-gaming-blog/posts/get-started-with-compute-shaders
+const compute = /* glsl */ `#version 310 es
+  uniform float radius;
+
+  struct Vector3f {
+    float x;
+    float y;
+    float z;
+    float w;
+  };
+
+  struct AttribData {
+    Vector3f v;
+    Vector3f c;
+  };
+
+  layout(std140, binding = 0) buffer destBuffer {
+    AttribData data[];
+  } outBuffer;
+
+  // TODO: how to represent in AST?
+  layout (local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+
+  void main() {
+    ivec2 storePos = ivec2(gl_GlobalInvocationID.xy);
+
+    uint gWidth = gl_WorkGroupSize.x * gl_NumWorkGroups.x;
+    uint gHeight = gl_WorkGroupSize.y * gl_NumWorkGroups.y;
+    uint gSize = gWidth * gHeight;
+
+    uint offset = storePos.y * gWidth + storePos.x;
+
+    float alpha = 2.0 * 3.14159265359 * float(offset) / float(gSize);
+
+    outBuffer.data[offset].v.x = sin(alpha) * radius;
+    outBuffer.data[offset].v.y = cos(alpha) * radius;
+    outBuffer.data[offset].v.z = 0.0;
+    outBuffer.data[offset].v.w = 1.0;
+
+    outBuffer.data[offset].c.x = storePos.x / float(gWidth);
+    outBuffer.data[offset].c.y = 0.0;
+    outBuffer.data[offset].c.z = 1.0;
+    outBuffer.data[offset].c.w = 1.0;
+  }
+`
+
 describe('generator', () => {
   it('can handle GLSL', () => {
     const program = parse(glsl)
     const output = generate(program, { target: 'GLSL' })
     expect(output).toBe(minify(glsl))
+  })
+
+  it('can handle GLSL ES 3.1', () => {
+    const program = parse(compute)
+    const output = generate(program, { target: 'GLSL' })
+    expect(output).toBe(minify(compute))
+    console.log(compute)
   })
 })
